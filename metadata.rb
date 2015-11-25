4f65ee53d11a4f5bb4cd2f8293a67ae58f10727b
Brandon Raabe <brandon.raabe@ge.com>
2015-11-25 15:43:10 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures auditd'
 long_description 'Installs/Configures auditd'
-version '0.1.0'
+version '0.1.1'
 
 %w(centos debian redhat ubuntu).each do |os|
   supports os
