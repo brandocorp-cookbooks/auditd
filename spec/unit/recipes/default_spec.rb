02901a031728e5e18a0e9c6d886eee541404b05d
Brandon Raabe <brandocorp@gmail.com>
2015-11-25 15:42:04 -0700

    add chefspec coverage for both rhel and debian

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,18 +7,32 @@
 require 'spec_helper'
 
 describe 'auditd::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on a debian-family system' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      ChefSpec::ServerRunner.new do |node,server|
+        node.automatic['platform_family'] = 'debian'
+      end.converge(described_recipe)
     end
 
     it 'installs auditd' do
       expect(chef_run).to install_package('auditd')
+    end
+
+    it 'starts and enables the auditd service' do
+      expect(chef_run).to start_service('auditd')
+      expect(chef_run).to enable_service('auditd')
+    end
+  end
+
+  context 'When all attributes are default on a rhel-family system' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new do |node,server|
+        node.automatic['platform_family'] = 'rhel'
+      end.converge(described_recipe)
+    end
+
+    it 'installs auditd' do
+      expect(chef_run).to install_package('audit')
     end
 
     it 'starts and enables the auditd service' do
